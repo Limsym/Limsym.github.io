1ed5450031c3db6435b368e41182f0e524601d41
Limsym <57558883+Ling-0-ling@users.noreply.github.com>
2025-03-31 16:18:23 +0800

    Add files via upload

--- products.js
+++ products.js
@@ -4,63 +4,216 @@ const products = {
             name: "2-Methyl-3-Trifluoromethylaniline (MTA)",
             cas: "13335-87-6",
             spec: "≥98%",
-            package: "25kg/桶"
+            package: "25kg/drum"
         },
         {
             name: "D-Tryptophan",
             cas: "153-94-6",
             spec: "≥99%",
-            package: "25kg/桶"
+            package: "25kg/drum"
         },
         {
             name: "4-(Isopropylamino)butanol",
             cas: "13335-87-6",
             spec: "≥98%",
-            package: "200kg/桶"
+            package: "200kg/drum"
+        },
+        {
+            name: "1-Bromocyclobutanecarboxylic acid",
+            cas: "6232-47-1",
+            spec: "≥98%",
+            package: "25kg/drum"
+        },
+        {
+            name: "2-Amino-4-nitrobenzoic acid",
+            cas: "619-17-0",
+            spec: "≥99%",
+            package: "25kg/drum"
+        },
+        {
+            name: "(S)-(+)-1,2-Propanediol",
+            cas: "4254-14-2",
+            spec: "≥99%",
+            package: "25kg/drum"
+        },
+        {
+            name: "1-Bromo-2,3-difluorobenzene",
+            cas: "64248-48-0",
+            spec: "≥99%",
+            package: "25kg/drum"
+        },
+        {
+            name: "Dimetindene Maleate",
+            cas: "3614-69-5",
+            spec: "≥99%",
+            package: "5kg/drum"
+        },
+        {
+            name: "Caronic anhydride",
+            cas: "10371-85-8",
+            spec: "≥98%",
+            package: "25kg/drum"
+        },
+        {
+            name: "2-chloro-1,4-naphthoquinone",
+            cas: "2097-61-2",
+            spec: "≥99%",
+            package: "5kg/drum"
+        },
+        {
+            name: "Trans-4-(4-chlorophenyl)cyclohexanecarboxylic acid",
+            cas: "35704-29-9",
+            spec: "≥99%",
+            package: "25kg/drum"
+        },
+        {
+            name: "1-bromo-2-butyne",
+            cas: "5202-27-3",
+            spec: "≥98%",
+            package: "5kg/drum"
+        },
+        {
+            name: "2-chloromalonaldehyde",
+            cas: "56483-47-7",
+            spec: "≥99%",
+            package: "10kg/drum"
         }
-        // 更多产品数据将从Excel导入
     ],
     vitamins: [
         {
             name: "Vitamin B1 (Thiamine)",
             cas: "59-43-8",
-            spec: "≥98%",
-            package: "25kg/桶"
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
         },
         {
             name: "Vitamin B2 (Riboflavin)",
             cas: "83-88-5",
-            spec: "≥98%",
-            package: "25kg/桶"
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
         },
         {
             name: "Vitamin B6 (Pyridoxine)",
             cas: "65-23-6",
-            spec: "≥98%",
-            package: "25kg/桶"
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
+        },
+        {
+            name: "Vitamin B12 (Cyanocobalamin)",
+            cas: "68-19-9",
+            spec: "USP/EP/JP",
+            package: "1kg/drum"
+        },
+        {
+            name: "Vitamin C (Ascorbic Acid)",
+            cas: "50-81-7",
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
+        },
+        {
+            name: "Vitamin D3 (Cholecalciferol)",
+            cas: "67-97-0",
+            spec: "USP/EP/JP",
+            package: "5kg/drum"
+        },
+        {
+            name: "Vitamin E (Tocopherol)",
+            cas: "59-02-9",
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
+        },
+        {
+            name: "Vitamin K1 (Phylloquinone)",
+            cas: "84-80-0",
+            spec: "USP/EP/JP",
+            package: "5kg/drum"
+        },
+        {
+            name: "Biotin (Vitamin B7)",
+            cas: "58-85-5",
+            spec: "USP/EP/JP",
+            package: "1kg/drum"
+        },
+        {
+            name: "Folic Acid (Vitamin B9)",
+            cas: "59-30-3",
+            spec: "USP/EP/JP",
+            package: "5kg/drum"
         }
-        // 更多产品数据将从Excel导入
     ],
     aminoAcids: [
         {
             name: "L-Lysine",
             cas: "56-87-1",
-            spec: "≥98%",
-            package: "25kg/桶"
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
         },
         {
             name: "L-Methionine",
             cas: "63-68-3",
-            spec: "≥98%",
-            package: "25kg/桶"
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
         },
         {
             name: "L-Threonine",
             cas: "72-19-5",
-            spec: "≥98%",
-            package: "25kg/桶"
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
+        },
+        {
+            name: "L-Tryptophan",
+            cas: "73-22-3",
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
+        },
+        {
+            name: "L-Phenylalanine",
+            cas: "63-91-2",
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
+        },
+        {
+            name: "L-Valine",
+            cas: "72-18-4",
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
+        },
+        {
+            name: "L-Arginine",
+            cas: "74-79-3",
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
+        },
+        {
+            name: "L-Glutamine",
+            cas: "56-85-9",
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
+        },
+        {
+            name: "Glycine",
+            cas: "56-40-6",
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
+        },
+        {
+            name: "L-Histidine",
+            cas: "71-00-1",
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
+        },
+        {
+            name: "L-Isoleucine",
+            cas: "73-32-5",
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
+        },
+        {
+            name: "L-Leucine",
+            cas: "61-90-5",
+            spec: "USP/EP/JP",
+            package: "25kg/drum"
         }
-        // 更多产品数据将从Excel导入
     ]
 };
 
